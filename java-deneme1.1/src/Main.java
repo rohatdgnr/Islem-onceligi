d01c73aa2d50bc3b60b368698152616b5c245d71
Rohat <159317933+rohatdgnr@users.noreply.github.com>
2024-02-20 12:31:44 +0300

    Update Main.java

--- java-deneme1.1/src/Main.java
+++ java-deneme1.1/src/Main.java
@@ -1,17 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Scanner;
+
+public class  Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Kullanıcıdan 3 sayıyı al
+        System.out.print("Lütfen birinci sayıyı girin: ");
+        double a = scanner.nextDouble();
+        System.out.print("Lütfen ikinci sayıyı girin: ");
+        double b = scanner.nextDouble();
+        System.out.print("Lütfen üçüncü sayıyı girin: ");
+        double c = scanner.nextDouble();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // İşlemi gerçekleştir ve sonucu ekrana yazdır
+        double sonuc = a + b * c - b;
+        System.out.println("İşlem sonucu: " + sonuc);
+
+
     }
 }
